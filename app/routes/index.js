d6b55765319ace959e09c10739abd81da274b7b7
bryonyoni <bryonyoni@gmail.com>
2017-04-11 12:57:36 +0300

    added styling
    added styling to the images and details of each bike

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-  return this.store.findAll('bike');
+  return this.store.query('bike',{
+    orderBy:'manufacturer',
+    equalTo:'yamaha-motors'
+  });
 },
 });
