e6cfee80a59b6ebb4e8a08608e4b3a7b2db3abb6
bryonyoni <bryonyoni@gmail.com>
2017-04-11 14:57:22 +0300

    arranged the images and details.
    aranged everythingin the bike section of the webpage

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-  return this.store.query('bike',{
-    orderBy:'manufacturer',
-    equalTo:'yamaha-motors'
-  });
+  return this.store.findAll('bike');
 },
 });
