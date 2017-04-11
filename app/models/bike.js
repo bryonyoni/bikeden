e08fd8e622f1aa0ce4c12ff33f17f499c786dc4d
jaden-lesuk <jadenlesuk12@gmail.com>
2017-04-11 11:43:43 +0300

    Added bootstrap

--- app/models/bike.js
+++ app/models/bike.js
@@ -1,16 +1,16 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  bike-photo-1: DS.attr(),
-  bike-photo-2: DS.attr(),
-  bike-photo-3: DS.attr(),
-  name: DS.attr(),
-  engine-type: DS.attr(),
-  transmission: DS.attr(),
-  price: DS.attr(),
-  description: DS.attr(),
-  year-of-manufacture: DS.attr(),
-  details: DS.attr(),
-  details-2: DS.attr(),
-  manufacturer: DS.attr(),
+	// bike-photo-1: DS.attr(),
+	// bike-photo-2: DS.attr(),
+	// bike-photo-3: DS.attr(),
+	// name: DS.attr(),
+	// engine-type: DS.attr(),
+	// transmission: DS.attr(),
+	// price: DS.attr(),
+	// description: DS.attr(),
+	// year-of-manufacture: DS.attr(),
+	// details: DS.attr(),
+	// details-2: DS.attr(),
+	// manufacturer: DS.attr(),
 });
